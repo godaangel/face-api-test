ac7f504d2f5a2a58a8d6b8b29c1da68599319c7e
warrenyang <warrenyang@tencent.com>
2020-01-05 18:08:03 +0800

    update

--- src/index.js
+++ src/index.js
@@ -6,16 +6,71 @@ import Ctp from '@/lib/console-to-page'; // 导入输出工具
 Ctp('欢迎使用es6代码编辑测试');
 
 import * as faceapi from 'face-api.js';
+import {fabric} from 'fabric'
 
-console.log(faceapi)
+let canvasContext = document.getElementById("canvas").getContext("2d")
+var video = document.getElementById('video')
 
 faceapi.nets.ssdMobilenetv1.load('/models').then(() => {
-    let input = document.getElementById('images')
+    faceapi.nets.faceLandmark68Net.load('/models').then(() => {
+        faceapi.nets.tinyFaceDetector.load('/models').then(() => {
+            openCamera()
+        })
+    })
+})
 
-    faceapi.detectAllFaces(input).then((res) => {
+function startDetect() {
+    let input = document.getElementById('video')
+    faceapi.detectAllFaces(input).withFaceLandmarks().then((res) => {
         console.log(res)
+        let landmarks = res[0].landmarks
+        const rightEyebrowPoints = landmarks.getRightEyeBrow();
+        const leftEyebrowPoints = landmarks.getLeftEyeBrow();
+        const outlinePoints = landmarks.getJawOutline();
+
+        console.log(rightEyebrowPoints, leftEyebrowPoints, outlinePoints)
+        
+        drawBox(res[0].detection)
     }).catch((err) => {
         console.log(err)
     })
-})
+}
+
+function openCamera() {
+    navigator.mediaDevices.getUserMedia({ audio: false, video: {
+        width: 300,
+        height: 200
+    }})
+    .then(function(stream) {
+      /* 使用这个stream stream */
+      console.log('----', stream)
+      video.srcObject = stream;
+      video.onloadedmetadata = function(e) {
+        video.play();
+        drawCanvas()
+      };
+    })
+    .catch(function(err) {
+      /* 处理error */
+    });
+}
+
+function drawCanvas() {
+    startDetect()
+    setTimeout(() => {
+        drawCanvas()
+    }, 34);
+}
+
+function drawBox(res) {
+    let {x, y, width, height} = res.box
+    const getFaceRadian = (jawPos, midPointOfEyebrows) =>
+    Math.PI - Math.atan2(jawPos.x - midPointOfEyebrows.x, jawPos.y - midPointOfEyebrows.y);
+
+    canvasContext.drawImage(video, 0, 0, 300, 200);
+    canvasContext.strokeStyle = "red";
+    canvasContext.lineWidth = 4;
+    canvasContext.strokeRect(x, y, width, height);
+}
+
 
